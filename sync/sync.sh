a726c86c624d67ff2eac46314dcb63dd0aaf6848
Tommaso <sikor666@gmail.com>
2019-08-16 14:10:08 +0200

    Add synchronize script

---
+++ sync/sync.sh
@@ -0,0 +1,1 @@
+rsync --recursive --no-perms --no-owner --no-group --verbose --delete --exclude='.*' --exclude='build/' /mnt/d/Disco/Programing/GIT/olive/ pi@192.168.1.101:/home/pi/Workspace/git/olive/
